1a09eca70b4da1534671bf5905d2ea5d16c26bc1
Ross Gravitte <Rbgravitte@gmail.com>
2018-11-13 13:48:46 -0500

    mongoose project start

--- recipes.js
+++ recipes.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const data = require('./data.js');
+const recipeSchema = new Schema()({
+  title : {type: String, required: true, unique: true},
+  level : {type: String, enum: ["easy Peasy", "amateur chef", "utrapro chef"]},
+  ingredients : {enum: []},
+  cuisine : {type: String, required: true},
+  dishType : {type: String, enum: ["Breakfast", "Dish", "Snack", "Drink", "Dessert", "Other"]},
+  image : {type: String, default: URL("https://images.media-allrecipes.com/images/75131.jpg")},
+  duration : {type: Number, min: 0},
+  creator : String,
+  created : {type: Date, default: new Date()}
+})
 
-mongoose.connect('mongodb://localhost/recipeApp')
-  .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+const Recipes = mongoose.model('Recipes', recipeSchema);
+
+module.exports(Recipes);
+
